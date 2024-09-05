6356a21036eaa92319564f22e408e2f039f4eed4
Wesley <wesmith314@gmail.com>
2024-09-04 21:31:19 -0700

    gets TypeScript setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+import './globals.css'
+
+import { useState, MouseEvent } from "react";
 
 export default function Home() {
+  const [isPushed, setIsPushed] = useState<boolean>(false);
+
+  const colorChange = (e: MouseEvent<HTMLButtonElement>): void => {
+    setIsPushed(prevState => !prevState);
+  };
+
   return (
-    <main >
+    <main>
+      <h1 className={isPushed ? "red" : "blue"} onClick={colorChange}>
+        Hello World
+      </h1>
     </main>
   );
 }
+
