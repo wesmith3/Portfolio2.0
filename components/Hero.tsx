8ae84b7fa45f6ece9e619437dbfd7de9533c9654
Wesley <wesmith314@gmail.com>
2024-09-10 08:45:14 -0700

    adds headings to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 function Hero() {
   return (
@@ -23,7 +24,13 @@ function Hero() {
             <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
                 Dynamic Web Magic with Next.JS
             </h2>
-
+            <TextGenerateEffect 
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless Experiences"
+            />
+            <p className="text-center md:tracking-wider mb-4 text-small md:text-lg lg:text-2xl">
+                Hi I&apos;m Wesley, a Full Stack Developer based in Seattle, WA
+            </p>
         </div>
 
       </div>
--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect } from "react";
 import { motion, stagger, useAnimate } from "framer-motion";
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/cn";
 
 export const TextGenerateEffect = ({
   words,
@@ -15,7 +15,7 @@ export const TextGenerateEffect = ({
   duration?: number;
 }) => {
   const [scope, animate] = useAnimate();
-  let wordsArray = words.split(" ");
+  const wordsArray = words.split(" ");
   useEffect(() => {
     animate(
       "span",
