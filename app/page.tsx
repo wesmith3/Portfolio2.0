a36e2624803a583b59c3f481a5accefafa844834
Wesley <wesmith314@gmail.com>
2024-09-10 20:51:28 -0700

    fixes navbar imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import FloatingNav from "@/components/ui/FloatingNav";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav />
         <Hero />
       </div>
     </main>
