717cc628a2d300c4f9413935a590a21353046ef0
Wesley <wesmith314@gmail.com>
2024-09-10 21:44:47 -0700

    imports data into index

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
 
 function Grid() {
   return (
     <section id="about">
       <BentoGrid>
-        {[{ title: 'Title1', description: 'Desc1', id: "1"}].map((item) => (
-            <BentoGridItem
-                id={item.id}
-                key={item.id}
-                title={item.title}
-                description={item.description}
-                />
+        {gridItems.map((item) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
         ))}
       </BentoGrid>
     </section>
-  )
+  );
 }
 
-export default Grid
+export default Grid;
