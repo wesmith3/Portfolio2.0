29963bdff62baa238ad5ebf446fabbef17f818f2
Wesley <wesmith314@gmail.com>
2024-09-11 15:16:10 -0700

    adds tech stack

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { Globe } from "./Globe";
+import { GlobeDemo } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -42,7 +44,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
       style={{
@@ -74,6 +76,7 @@ export const BentoGridItem = ({
             <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
           </BackgroundGradientAnimation>
         )}
+
         <div className={cn(
           titleClassName, 'group-hover:/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
         )}>
@@ -82,9 +85,32 @@ export const BentoGridItem = ({
           </div>
           <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
           {title}
-        </div>
-        </div>
+          </div>
+
+        {id === 2 && <GlobeDemo />}
+
+        {id === 3 && (
+          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex flex-col gap-3 lg:gap-8">
+              {['TypeScript', 'React', 'Next.js'].map((item) => (
+                <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-75 rounded-lg text-center bg-[#10132E]">
+                  {item}
+                </span>
+              ))}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]"/>
+            </div>
+            <div className="flex flex-col gap-3 lg:gap-8">
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]"/>
+              {['Node.js', 'MongoDB', 'AWS'].map((item) => (
+                <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-75 rounded-lg text-center bg-[#10132E]">
+                  {item}
+                </span>
+              ))}
+            </div>
+          </div> 
+        )}
       </div>
+    </div>
     </div>
   );
 };
