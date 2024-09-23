ddc7cdbf98de1973356099ec2ade96643d3b4aea
Wesley <wesmith314@gmail.com>
2024-09-23 15:27:16 -0700

    adds footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Clients from "@/components/Clients";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -16,6 +17,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Footer />
       </div>
     </main>
   );
