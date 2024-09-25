be3bf900677035d7e793fe1b8a1b3a658ed8f789
Wesley <wesmith314@gmail.com>
2024-09-24 22:22:08 -0700

    adds links to contact logos

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
+import { IconType } from 'react-icons';
 import { SiLinkedin, SiGithub, SiMedium } from 'react-icons/si';
 
-const Contact = () => {
-  const technologies = [
-    { icon: SiLinkedin, name: 'LinkedIn', color: '#0A66C2' },
-    { icon: SiGithub, name: 'GitHub', color: '#181717' },
-    { icon: SiMedium, name: 'Medium', color: '#FFFFFF' },
+interface Technology {
+  icon: IconType;
+  name: string;
+  color: string;
+  url: string;
+}
+
+const Contact: React.FC = () => {
+  const technologies: Technology[] = [
+    { icon: SiLinkedin, name: 'LinkedIn', color: '#0A66C2', url: 'https://www.linkedin.com/in/wesmith314/' },
+    { icon: SiGithub, name: 'GitHub', color: '#FFFFFF', url: 'https://github.com/wesmith3' },
+    { icon: SiMedium, name: 'Medium', color: '#FFFFFF', url: 'https://medium.com/@wesmith314' },
   ];
 
   return (
-    <div className="flex justify-center space-x-4 mt-4">
+    <div className="flex justify-center space-x-4 mt-10">
       {technologies.map((tech) => (
-        <div key={tech.name} className="flex flex-col items-center">
-          <tech.icon 
-            className="text-3xl" 
+        <a
+          key={tech.name}
+          href={tech.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex flex-col items-center no-underline"
+        >
+          <tech.icon
+            className="text-3xl transition-transform hover:scale-110"
             style={{ color: tech.color }}
           />
           <span className="text-xs mt-1 dark:text-gray-300">{tech.name}</span>
-        </div>
+        </a>
       ))}
     </div>
   );
